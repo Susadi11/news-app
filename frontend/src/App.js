d68556353962ccd525996060e866665fe8262ba4
Susadi11 <susadisandanima@gmail.com>
2024-11-22 21:52:42 +0530

    test commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,10 +3,14 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NewsHeadlines from './components/NewsHeadlines';
 import NewsArticleDetail from './components/NewsArticleDetail';
 
+//This road to compass
+
+
 const App = () => {
   return (
     <Router>
       <div className="bg-gray-100 min-h-screen">
+        
         <Routes>
           <Route path="/" element={<NewsHeadlines />} />
           <Route path="/article/:articleUrl" element={<NewsArticleDetail />} />
